86f14b821b0f4981b0f20ba48ab94dc88e3e2a53
AimenAsif <aimenasif257@gmail.com>
2024-03-27 11:29:21 -0700

    finished project

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.loginapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username =findViewById(R.id.username_input);
+        password =findViewById(R.id.password_input);
+        login_btn =findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(this::onClick);
+    }
+
+    private void onClick(View view) {
+        if (username.getText().toString().equals("user") && password.getText().toString().equals("3434")) {
+            Toast.makeText(MainActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 }
